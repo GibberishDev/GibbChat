c9996e9fc354cda25f47b0cdffff3ff058040c32
GibberishDev <gibberishdevelopment@gmail.com>
2026-06-16 21:33:55 +0300

    Linux build

--- app/index.js
+++ app/index.js
@@ -1,3 +1,20 @@
-const { app } = require('electron')
-console.log("A")
-app.quit()
+const { app, BrowserWindow } = require('electron')
+const path = require("path")
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+	icon: path.join(__dirname,"icon.ico")
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
